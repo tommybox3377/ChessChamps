100a786483c20169ed6b3bbb7aa7cc5958adfaec
twmar <twmaryniak@gmail.com>
2023-03-11 11:08:12 -0500

    very rough first version of multiplayer working

--- backend/chess_engine/chess_engine.py
+++ backend/chess_engine/chess_engine.py
@@ -24,18 +24,25 @@ def update_game(input_json):
         else:
             game_data['termination'] = 'draw'
 
-    game_data['legal_moves'] = list(board.generate_legal_moves())
+    legal_moves = [str(x) for x in list(board.generate_legal_moves())]
+    game_data['legal_moves'] = legal_moves
+
+    if board.turn:
+        game_data["turn"] = "w"
+    else:
+        game_data["turn"] = "b"
 
     json_data = json.dumps(game_data)
     return json_data
 
-
 incoming_json = {
     "game_id": "abc",
     "moves": ["a3b8"],
-    "next_to_move": 0
+    "next_to_move": 0,
+    "fen":"rnbqkb1r/pppp1p1p/5n2/4p1p1/8/5NP1/PPPPPPBP/RNBQK2R w KQkq - 0 1"
 }
 
+h = update_game(json.dumps(incoming_json))
 resp_json = {
     "game_id": "abc",
     "legal_moves": ["a3bg", "g6h7"],
@@ -51,4 +58,10 @@ init_dict = {
     "moves": ["a3bg", "g6h7", "a3b7"],
     "board_id": 0,
     "termination": "draw",
+}
+
+game = {
+    "game_over": False,
+    "turn": "w",
+    "legal_moves":[],
 }
